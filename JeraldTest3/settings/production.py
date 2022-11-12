6dda557abbbcf6ea3b011e18251942d21e4c10e8
jerald011003 <coninegaming@gmail.com>
2022-11-12 08:44:06 +0800

    kldsajhklas

--- JeraldTest3/settings/production.py
+++ JeraldTest3/settings/production.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, "templates", "templates/auth"),
+            os.path.join(BASE_DIR, "templates"),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
