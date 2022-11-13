1e3d7c6fc2b81e9a7a3945cd0a981b0036dab382
jerald011003 <coninegaming@gmail.com>
2022-11-13 19:56:20 +0800

    asdsda

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,9 @@ from django.db import models
 from .models import Order
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
