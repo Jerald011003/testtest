857b19fcc48a4f9ce87f504b0085fbd8a28ea397
jerald011003 <coninegaming@gmail.com>
2022-11-13 19:03:54 +0800

    asddsa

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,7 +24,7 @@ class OrderForm(ModelForm):
 class CreateUserForm(UserCreationForm):
         class Meta:
             model = User
-            fields = ['username', 'email', 'password1', 'password2']
+            fields = ['username', 'email', 'password1']
 
 
 class CustomerForm(ModelForm):
