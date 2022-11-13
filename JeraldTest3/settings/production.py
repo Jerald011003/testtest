4df41f2aa652171abc5e83ec63062a89279ffb82
jerald011003 <coninegaming@gmail.com>
2022-11-13 17:49:58 +0800

    asdsda

--- JeraldTest3/settings/production.py
+++ JeraldTest3/settings/production.py
@@ -132,13 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static/../../static_my_project", ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/../../static_cdn", "static_root")
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/../../static_cdn", "media_root")
 # Default primary key field type
