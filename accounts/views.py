40ee4b7bdacd16d61a454c6495e97c7abf26b001
jerald011003 <coninegaming@gmail.com>
2022-11-13 22:12:08 +0800

    asddas

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,26 @@
-from django.shortcuts import render, redirect
-from accounts.forms import EmployeeForm
-from accounts.models import Employee
-
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-
-from django.http import HttpResponse
 
 from .models import *
 from accounts.forms import OrderForm, CreateUserForm
 from django.forms import inlineformset_factory
 from .filters import OrderFilter, CustomerFilter
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from accounts.forms import *
 from .forms import *
 
+
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     else:
-        form = CreateUserForm()
+        form = UserCreationForm()
         if request.method == 'POST':
-            form = CreateUserForm(request.POST)
+            form = UserCreationForm(request.POST)
             if form.is_valid():
                 form.save()
                 user = form.cleaned_data.get('username')
