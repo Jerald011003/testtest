4d6c021866bacf957931db1842e119360731f8fa
jerald011003 <coninegaming@gmail.com>
2022-11-13 20:44:17 +0800

    asddsa

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
@@ -9,3 +10,7 @@ admin.site.register(ProductForCustomer)
 admin.site.register(Order)
 admin.site.register(Tag)
 
+class MyUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Departmet', {'fields': ('department',)}),
+    )
